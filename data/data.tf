85b3ec5470562ae9245c4f42190c3771734e071f
shiva-charan-git <shivacharan021@gmail.com>
2023-06-04 20:13:06 +0530

    M

--- data/data.tf
+++ data/data.tf
@@ -4,6 +4,7 @@ data "aws_ami" "ami" {
   owners           = ["290972336566"]
 }
 
-output "ami_id" {
-  value = data.aws_ami.ami.image_id
+output "ami" {
+  value = data.aws_ami.ami.image_type
+  
 }
