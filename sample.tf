b8e4b7518126eb7f42ef6789636751f4a35049e8
shiva-charan-git <shivacharan021@gmail.com>
2023-06-04 19:32:57 +0530

    M

--- sample.tf
+++ sample.tf
@@ -1,8 +1,4 @@
 resource "aws_instance" "ec2" {
- ami = "ami-0b5a2b5b8f2be4ec2"  
- instance_type = "t3.micro"
- vpc_security_group_ids = [ "sg-0827c01ee4ba4d8aa" ]
- tags = {
-    Name = "test"
- }
+   ami = "ami-0b5a2b5b8f2be4ec2"
+   vpc_security_group_ids = ["sg-0827c01ee4ba4d8aa"]
 }
