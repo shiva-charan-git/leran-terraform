42b46f64d3eb4aae924719a8b9d131f1b0d29dda
shiva-charan-git <shivacharan021@gmail.com>
2023-06-05 19:55:18 +0530

    M

--- vars/var.tf
+++ vars/var.tf
@@ -18,3 +18,32 @@ variable "cli" {}
 output "cli" {
   value = var.cli
 }
+
+variable "sample5" {
+  default = "Hello"
+}
+
+
+variable "sample6" {
+default = {
+  "Hello",
+  1000,
+  true,
+  "World"
+}
+}
+
+variable "sample7" {
+  default = {
+    string = "Hello"
+    number = 100,
+    boolean = true
+  }
+  
+}
+
+output "types" {
+
+  value = "variable sample5 is ${var.sample5}, first valiue of list is ${var.sample6[0]}, boolean value is ${var.smaple7[boolean]}"
+  
+}
