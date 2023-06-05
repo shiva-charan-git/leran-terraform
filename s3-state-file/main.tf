66178b84da391e0ba93399836b194f8226bea31c
shiva-charan-git <shivacharan021@gmail.com>
2023-06-06 00:37:59 +0530

    M

--- s3-state-file/main.tf
+++ s3-state-file/main.tf
@@ -6,7 +6,7 @@ terraform {
   }
 }
 
-bresource "aws_instance" "ec2" {
+resource "aws_instance" "ec2" {
    ami = "ami-03265a0778a880afb"
    instance_type = "t3.micro"
    vpc_security_group_ids = ["sg-0827c01ee4ba4d8aa"]
